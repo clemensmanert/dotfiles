8562f68fe25f0aff90e3570b887f2a8ef2605c1e
Clemens Manert <Clemens@Manert.de>
2016-11-21 20:29:00 +0100

    Init commit

---
+++ setup.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# Creates links into dotfiles
+
+#awesome
+ln -s $PWD/awesome ~/.config/awesome
+
+# compton
+ln -s $PWD/compton.conf  ~/.config/compton.conf
+
+#conkey
+ln -s $PWD/conkyrc_$HOSTNAME
+
+# emacs
+ln -s $PWD/emacs ~/.emacs.d
+
+# fish
+ln -s $PWD/fish ~/.config/fish
+
+#i3
+ln -s $PWD/i3 ~/.i3
+
+# khal
+ln -s $PWD/khal ~/.khal
+
+# mpd
+ln -s $PWD/mpdconf ~/.mpdconf
+
+# ranger
+ln -s $PWD/ranger ~/.config/ranger
+
+# tmux
+ln -s $PWD/tmux/tmux.conf   ~/.tmux.conf
+
+# vimprc
+ln -s $PWD/vimpcrc ~/.vimpcrc
+
+# xbinkeys
+ln -s $PWD/xbindkeysrc ~/.xbindkeysrc
+
+# zathura
+ln -s $PWD/zathura ~/.zathura
