f7be56341cdc0621a89b7fdaa1cfc7404e25ed1d
Clemens Manert <Clemens@Manert.de>
2016-11-21 20:08:30 +0100

    Removed unnecessary theme bindings. Use now helm for theme switching.

--- emacs/bindings.el
+++ emacs/bindings.el
@@ -4,20 +4,10 @@
 ;; Lisp
 (global-set-key (kbd "C-x e") 'eval-buffer)
 
-;; Switching color-theme
-(global-set-key [f8] 'my-theme-cycle-dark)
-(global-set-key [f9] 'my-theme-cycle-light)
-
-;; Jump to next section
-(global-set-key (kbd "M-<up>") 'move-text-up)
-(global-set-key (kbd "M-<down>") 'move-text-down)
- 
-;; auto complete current word
-;;(global-set-key [C-return] 'auto-complete)
+ ;; auto complete current word
 (global-set-key [C-return] 'company-complete)
 
 ;; Window control
-;; Create and remove windows.
 (global-set-key (kbd "M-3") 'split-window-horizontally)
 (global-set-key (kbd "M-2") 'split-window-vertically)
 (global-set-key (kbd "M-1") 'delete-other-windows)
@@ -32,15 +22,11 @@
 (global-set-key (kbd "C-M-<right>") 'windmove-right)
 (global-set-key (kbd "C-M-<left>") 'windmove-left)
 
-;; Multiple cursor 
-(global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
+;; Multiple cursor
 (global-set-key (kbd "C-<") 'mc/mark-next-like-this)
 (global-set-key (kbd "C->") 'mc/mark-previous-like-this)
 (global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
 (global-set-key (kbd "C-S-<mouse-1>") 'mc/add-cursor-on-click)
-
-;; List recent files
-(global-set-key (kbd "C-x C-r") 'recentf-open-files)
 
 ;; Fullscreen
 (global-set-key [f11] 'toggle-fullscreen)
@@ -50,8 +36,25 @@
 (define-key global-map (kbd "C-c q") 'vr/query-replace)
 (define-key global-map (kbd "C-c m") 'vr/mc-mark)
 
-;; Ispell dictionaries
-(global-set-key [f6] 'cycle-ispell-languages)
-
 ;; unset
 (global-unset-key (kbd "C-z"))
+
+;; Helm
+(global-set-key (kbd "M-x") 'helm-M-x)
+
+(global-set-key (kbd "<f6>") 'helm-themes)
+
+(global-set-key (kbd "C-x b") 'helm-buffers-list)
+(global-set-key (kbd "C-x C-f") 'helm-find-files)
+
+(global-set-key (kbd "C-x C-r") 'helm-recentf)
+(global-set-key (kbd "C-c C-f") 'helm-locate)
+
+(global-set-key (kbd "C-x C-g") 'helm-imenu)
+(global-set-key (kbd "M-RET") 'helm-company)
+
+;; uncomment-region
+(global-set-key (kbd "C-S-c") 'uncomment-region)
+(global-set-key (kbd "<f5>") 'compile)
+(global-set-key (kbd "M-.") 'semantic-ia-fast-jump)
+
