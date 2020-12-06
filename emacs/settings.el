72d276887a21e8c50649ed453616106c771f9c6e
Clemens Manert <Clemens@Manert.de>
2020-12-06 13:07:48 +0100

    Adds typescript support using tide

--- emacs/settings.el
+++ emacs/settings.el
@@ -22,6 +22,19 @@
 
 (setq backup-directory-alist `(("." . "~/.emacs_backups")))
 
+;; Typescript
+(defun setup-tide-mode ()
+  (interactive)
+  (tide-setup)
+  (flycheck-mode +1)
+  (setq flycheck-check-syntax-automatically '(save mode-enabled))
+  (eldoc-mode +1)
+  (tide-hl-identifier-mode +1)
+  ;; company is an optional dependency. You have to
+  ;; install it separately via package-install
+  ;; `M-x package-install [ret] company`
+  (company-mode +1))
+
 (set-language-environment "UTF-8")
 (set-default-coding-systems 'utf-8)
 
