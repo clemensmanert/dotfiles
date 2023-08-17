bb227b483990a4085b6402ad30efc811451096a7
Clemens Manert <Clemens@Manert.de>
2023-08-18 01:05:16 +0200

    Remove tide (typescript)

--- emacs/.emacs.d/settings.el
+++ emacs/.emacs.d/settings.el
@@ -26,19 +26,6 @@
 
 (setq backup-directory-alist `(("." . "~/.emacs_backups")))
 
-;; Typescript
-(defun setup-tide-mode ()
-  (interactive)
-  (tide-setup)
-  (flycheck-mode +1)
-  (setq flycheck-check-syntax-automatically '(save mode-enabled))
-  (eldoc-mode +1)
-  (tide-hl-identifier-mode +1)
-  ;; company is an optional dependency. You have to
-  ;; install it separately via package-install
-  ;; `M-x package-install [ret] company`
-  (company-mode +1))
-
 ;; robe on ruby
 (add-hook 'ruby-mode-hook 'robe-mode)
 (add-hook 'ruby-mode-hook 'yard-mode)
